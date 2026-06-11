15a27613a989160bccce9e86fe634be7a53d511b
Baljeet Kumar Yadav <baljeetydv0001@gmail.com>
2026-06-11 23:06:42 +0530

    Day 1 - Variables,Data Type, ConversionOperatons

---
+++ 01_basics/03_ConversionOperation.js
@@ -0,0 +1,35 @@
+let score = "223ab";
+let score1 = null;
+let score2 = undefined;
+ 
+console.log(typeof score);
+console.log(typeof (score));
+
+
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+console.log(score1);
+console.log(score2);
+
+
+
+let islogged = "";
+let islogged1 = "toy";
+let islogged2 = null;
+
+let booleanIsLogged = Boolean(islogged);
+let booleanIsLogged1 = Boolean(islogged1);
+let booleanIsLogged2 = Boolean(islogged2);
+
+console.log(booleanIsLogged);
+console.log(booleanIsLogged1);
+console.log(booleanIsLogged2);
+
+let someNumber = 33;
+let stringNumber = String(someNumber);
+console.log(typeof stringNumber);
+console.log(stringNumber);
+
+
+
