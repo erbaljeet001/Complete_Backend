e7e399ac1b4e57b32addca27c9c09603bb3edf80
Baljeet Kumar Yadav <baljeetydv0001@gmail.com>
2026-06-12 06:47:50 +0530

    updated conversion operation

--- 01_basics/03_ConversionOperation.js
+++ 01_basics/03_ConversionOperation.js
@@ -1,35 +1,87 @@
-let score = "223ab";
-let score1 = null;
-let score2 = undefined;
+// let score = "223ab";
+// let score1 = null;
+// let score2 = undefined;
  
-console.log(typeof score);
-console.log(typeof (score));
+// console.log(typeof score);
+// console.log(typeof (score));
 
 
-let valueInNumber = Number(score);
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
-console.log(score1);
-console.log(score2);
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+// console.log(score1);
+// console.log(score2);
 
 
 
-let islogged = "";
-let islogged1 = "toy";
-let islogged2 = null;
+// let islogged = "";
+// let islogged1 = "toy";
+// let islogged2 = null;
 
-let booleanIsLogged = Boolean(islogged);
-let booleanIsLogged1 = Boolean(islogged1);
-let booleanIsLogged2 = Boolean(islogged2);
+// let booleanIsLogged = Boolean(islogged);
+// let booleanIsLogged1 = Boolean(islogged1);
+// let booleanIsLogged2 = Boolean(islogged2);
 
-console.log(booleanIsLogged);
-console.log(booleanIsLogged1);
-console.log(booleanIsLogged2);
+// console.log(booleanIsLogged);
+// console.log(booleanIsLogged1);
+// console.log(booleanIsLogged2);
 
-let someNumber = 33;
-let stringNumber = String(someNumber);
-console.log(typeof stringNumber);
-console.log(stringNumber);
+// let someNumber = 33;
+// let stringNumber = String(someNumber);
+// console.log(typeof stringNumber);
+// console.log(stringNumber);
 
+
+// let value =3;
+// let negvalue = -value;
+// console.log(negvalue);
+
+// // arithmatic operation
+//  console.log("1"+"2")
+//  console.log(1+"2")
+//  console.log("1"+2+2)
+//  console.log(1+2+"2")
+
+
+// let num1, num2, num3;
+
+// num1 = num2 = num3 = 2 + 2;
+// console.log(num1)
+// console.log(num2)
+// console.log(num3)
+
+// let gameCounter = 100;
+// console.log(++gameCounter)
+// console.log(gameCounter++)
+// console.log(gameCounter)
+// gameCounter++;
+
+// console.log(gameCounter);
+
+// NORMAL CONVERSION
+// console.log(2>1);
+// console.log(2>=2);
+// console.log(2==2);
+// console.log(5 != 1);
+// console.log(1 == 1);
+// console.log(1 === 1);
+// console.log(10 == 1);
+// console.log(10 === 1);
+
+// Avoid this type of conversion
+
+// console.log(null > 0);
+// console.log(null == 0)
+// console.log(null >= 0)
+
+// console.log(undefined>=0)
+// console.log(undefined==0)
+// console.log(undefined >= 0)
+
+
+//=======
+
+console.log("2"===2)
+console.log("2"==2)
 
 
